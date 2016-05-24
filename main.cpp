10f6586207229ed0965ce368e1161673b6a79ef3
callumork <callumork@hotmail.com>
2016-05-24 09:29:04 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <time.h>
+#include <stack>
 
 using namespace std;
 
@@ -15,6 +16,14 @@ struct Data //for data to be passed to spreadsheet
 int main()
 {
     int choice;
+    int time;
+
+
+    vector <int> stackvect(10);
+    for (int i=1; i<11; i++)
+    {
+        stackvect [i-1]= i;
+    }
     do
     {
         cout << "Welcome! " << endl;
@@ -36,6 +45,49 @@ int main()
                 cin.ignore(666, '\n');
 
             }
+        }
+        if (choice==2)
+        {
+
+            cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+                <<"Initial Vector"<<endl;
+            for (int i=0; i<10; i++)
+            {
+                cout << stackvect [i]<< " ";
+            }
+            cout << "\n\nPOP function:\nInput code: (vector name).pop_back"<<endl;
+            stackvect.pop_back();
+            for (int i=0; i<(stackvect.size()); i++)
+            {
+                cout << stackvect [i]<< " ";
+            }
+            cout << "\n\nPush function:\nInput code: (vector name).push_back(11)"<<endl;
+            stackvect.push_back(11);
+            for (int i=0; i<(stackvect.size()); i++)
+            {
+                cout << stackvect [i]<< " ";
+            }
+            cout<< "\n\nEmpty function:\n\nInput code if((vector name).empty()==false)\ncout<<\"No\"\n\nIs the vector empty? ";
+            if (stackvect.empty()==false)
+                cout << "No";
+            cout<<endl;
+            for (int i=0; i<(stackvect.size()); i++)
+            {
+                cout << stackvect [i]<< " ";
+            }
+            cout<<" --> No effect on vector."<<endl;
+
+            cout << "\n\nTop function:\nInput code: std::stack<int> mystack \nmystack.push(10) \nmystack.push(20) \nstd::cout << \"mystack.top() is\" << mystack.top()"<<endl;
+            std::stack<int> mystack;
+            mystack.push(10);
+            mystack.push(20);
+            std::cout << "\n\nThe top is: " << mystack.top() << '\n';
+
+
+
+            cout<<"\n\nInput anything to continue: ";
+            cin>>time;
+            cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
         }
 
         if (choice > 2 | choice < 0)
